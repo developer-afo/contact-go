e860a8507fd9d86e37ac706f465df4fb7785c4c8
Afolabi Olayinka <folabiolayinka@gmail.com>
2023-01-04 21:04:52 +0100

    fix jwt bug

--- security/security.go
+++ security/security.go
@@ -5,21 +5,21 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"net/http"
 	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
+	"github.com/valyala/fasthttp"
 )
 
 const (
-	// JwtKey : const - change this key
-	JwtKey = "e9011a4990393f2a7922eaa30f1b48fa"
+	// JWT_KEY : const
+	JWT_KEY = "3085e4c8a97e3fe2bc4d6a5b95d525b3"
 )
 
 // CreateToken : function
-func CreateToken(userID string) (string, error) {
+func CreateToken(organizationID string, userID string) (string, error) {
 	// Create token
 	token := jwt.New(jwt.SigningMethodHS256)
 
@@ -29,38 +29,41 @@ func CreateToken(userID string) (string, error) {
 	claims := token.Claims.(jwt.MapClaims)
 	claims["sub"] = userID
 	claims["iat"] = time.Now().Unix()
+	claims["ver"] = 1 //version
 	claims["exp"] = time.Now().Add(time.Minute * 15).Unix()
 
 	// Generate encoded token and send it as response.
 	// The signing string should be secret (a generated UUID works too)
-	t, err := token.SignedString([]byte(JwtKey))
+	_token, err := token.SignedString([]byte(JWT_KEY))
 	if err != nil {
 		return "", err
 	}
 
-	return t, nil
+	return _token, nil
 }
 
 // ExtractUserID : function
-func ExtractUserID(token *jwt.Token) (uid uuid.UUID, err error) {
+func ExtractUserID(r *fasthttp.Request) (uid uuid.UUID, err error) {
+
+	token, err := ExtractTokenObject(r)
+
 	claims := token.Claims.(jwt.MapClaims)
-	var userID string
+
 	if claims["sub"] == nil {
-		return uid, errors.New("invalid token: user id not found")
-	} else {
-		userID = claims["sub"].(string)
+		return uuid.Nil, errors.New("invalid token: user id not found")
 	}
+	userID := claims["sub"].(string)
 	return uuid.Parse(userID)
 }
 
 // TokenValid : function
-func TokenValid(r *http.Request) error {
+func TokenValid(r *fasthttp.Request) error {
 	tokenString := ExtractToken(r)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte(JwtKey), nil
+		return []byte(JWT_KEY), nil
 	})
 	if err != nil {
 		return err
@@ -72,17 +75,34 @@ func TokenValid(r *http.Request) error {
 }
 
 // ExtractToken : function
-func ExtractToken(r *http.Request) string {
-	keys := r.URL.Query()
-	token := keys.Get("token")
+func ExtractToken(r *fasthttp.Request) string {
+
+	keys := r.URI().QueryArgs()
+	token := string(keys.Peek("token"))
 	if token != "" {
 		return token
 	}
-	bearerToken := r.Header.Get("Authorization")
+
+	bearerToken := string(r.Header.Peek("Authorization"))
 	if len(strings.Split(bearerToken, " ")) == 2 {
 		return strings.Split(bearerToken, " ")[1]
 	}
 	return ""
+}
+
+// ExtractTokenObject : function
+func ExtractTokenObject(r *fasthttp.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(JWT_KEY), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, err
 }
 
 // Pretty display the claims licely in the terminal
@@ -94,4 +114,8 @@ func Pretty(data interface{}) {
 	}
 
 	fmt.Println(string(b))
+}
+
+func GetSecret() string {
+	return JWT_KEY
 }
